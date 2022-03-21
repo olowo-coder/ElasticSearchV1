04e0e0d257bf8c6679b48acb9ec7a3454028e4cf
olowo-coder <olowofela.olanrewaju@decagon.dev>
2022-03-21 21:08:49 +0100

    configuration

---
+++ src/main/java/com/example/springelasticsearchv1/SpringElasticSearchV1Application.java
@@ -0,0 +1,13 @@
+package com.example.springelasticsearchv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringElasticSearchV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringElasticSearchV1Application.class, args);
+    }
+
+}
